c8e150689f8a82b94da9b20a7ab0e699f35b9701
Denis <160841197+DenP0821@users.noreply.github.com>
2024-12-16 17:38:43 +0300

    Final update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Доступ к элементам для их стилизации
 const stylingNavText = document.querySelectorAll(".nav-text");
 const stylingSVG = document.querySelectorAll("svg");
 
+// Стилизация элементов навигации при наведении
 stylingNavText.forEach((item) => {
   item.addEventListener("mouseover", () =>
     item.classList.add("styling-nav-text")
@@ -12,9 +14,34 @@ stylingNavText.forEach((item) => {
   );
 });
 
+// Стилизация элементов SVG при наведении (подвал сайта)
 stylingSVG.forEach((item) => {
   item.addEventListener("mouseover", () => item.classList.add("styling-svg"));
 });
 stylingSVG.forEach((item) => {
   item.addEventListener("mouseout", () => item.classList.remove("styling-svg"));
 });
+
+// Удаление хэша из URL и прокрутка страницы к началу (при перезагрузке)
+function removeHash() {
+  history.pushState(
+    "",
+    document.title,
+    window.location.pathname + window.location.search
+  );
+}
+
+function scrollToBegin() {
+  if (history.scrollRestoration) {
+    history.scrollRestoration = "manual";
+  } else {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }
+}
+
+if (window.location.reload) {
+  removeHash();
+  scrollToBegin();
+}
